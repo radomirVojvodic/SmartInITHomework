3664154219215a7c0893027ff5e706c563d878be
Radomir Vojvodic <radomir.vojvodic@icloud.com>
2023-03-22 02:43:10 +0100

    My 4 Homework

---
+++ RentalCars/RentalCars/Controllers/CarsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RentalCars.Models;
+
+namespace RentalCars.Controllers
+{
+    public class CarsController : Controller
+    {
+        // GET: Cars
+        public ActionResult Index()
+        {
+            var cars = GetCars();
+            return View(cars);
+        }
+
+        private IEnumerable<Car> GetCars()
+        {
+            var cars = new List<Car>
+            {
+                new Car{ID=1,Manufacture="Audi", Model="A6", Year=2010},
+                new Car{ID=2, Manufacture="BMW", Model="X5", Year=2018},
+                new Car{ID=3, Manufacture="Hyndai", Model="i30", Year=2023}
+            };
+
+            return cars;
+        }
+
+
+    }
+}
---
+++ RentalCars/RentalCars/Models/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RentalCars.Models;
+
+namespace RentalCars.Models
+{
+    public class Car
+    {
+
+        public int ID { get; set; }
+        public string Model { get; set; }
+        public string Manufacture { get; set; }
+        public int Year { get; set; }
+    
+    }
+}
